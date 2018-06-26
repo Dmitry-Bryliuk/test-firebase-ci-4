08d42be1cced272d33edb82ec245d6371f11f5bb
Dmitry-Bryliuk <dmitry.bryliuk@evote.com>
2018-06-26 16:01:57 +0300

    return original realtime db functions

--- functions/index.js
+++ functions/index.js
@@ -11,7 +11,25 @@ admin.initializeApp({
     databaseURL: "https://test-ci-b56e8.firebaseio.com"
 });
 
-// firestore
+// original realtime db functions from sample: https://github.com/Firebase/functions-samples/tree/master/quickstarts/uppercase/functions/test/
+// comments stripped
+
+exports.addMessage = functions.https.onRequest((req, res) => {
+    const original = req.query.text;
+    return admin.database().ref('/messages').push({ original: original }).then((snapshot) => {
+        return res.redirect(303, snapshot.ref.toString());
+    });
+});
+
+exports.makeUppercase = functions.database.ref('/messages/{pushId}/original')
+    .onCreate((snapshot, context) => {
+        const original = snapshot.val();
+        console.log('Uppercasing', context.params.pushId, original);
+        const uppercase = original.toUpperCase();
+        return snapshot.ref.parent.child('uppercase').set(uppercase);
+    });
+
+// firestore functions
 
 exports.addMessageFirestore = functions.https.onRequest((req, res) => {
     const original = req.query.text;
